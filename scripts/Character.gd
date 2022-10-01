7416ec1018f1689ca116b71a506fb4d08c7c5afb
rhyzeng <zeng.raymondhy@gmail.com>
2022-10-01 14:25:52 +1000

    basic customer spawner

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -9,32 +9,35 @@ extends CharacterBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 # Called every physics frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-    velocity = Vector3.ZERO
+	velocity = Vector3.ZERO
 
-    var direction = Vector3.ZERO
-    if Input.is_action_pressed("move_forward"):
-        direction += Vector3.FORWARD
-    if Input.is_action_pressed("move_backward"):
-        direction += Vector3.BACK
-    if Input.is_action_pressed("move_left"):
-        direction += Vector3.LEFT
-    if Input.is_action_pressed("move_right"):
-        direction += Vector3.RIGHT
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("move_forward"):
+		direction += Vector3.FORWARD
+	if Input.is_action_pressed("move_backward"):
+		direction += Vector3.BACK
+	if Input.is_action_pressed("move_left"):
+		direction += Vector3.LEFT
+	if Input.is_action_pressed("move_right"):
+		direction += Vector3.RIGHT
 
-    velocity = direction.normalized() * speed
-    velocity = velocity.rotated(Vector3.UP, rotation.y)
+	velocity = direction.normalized() * speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
-    move_and_slide()
+	move_and_slide()
 
 
 func _input(event):
-    if event is InputEventMouseMotion:
-        var movement = event.relative
-        camera.rotation.x -= movement.y * mouse_sensitivity
-        camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
-        rotation.y -= movement.x * mouse_sensitivity
+	if event is InputEventMouseMotion:
+		var movement = event.relative
+		camera.rotation.x -= movement.y * mouse_sensitivity
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+		rotation.y -= movement.x * mouse_sensitivity
+
+func _on_customer_timer_timeout():
+	pass # Replace with function body.
